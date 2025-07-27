46f92f6b6c8df54e2e683ba91a66d3cb001d5158
paul osamai <paul.osamai8@gmail.com>
2025-07-27 08:04:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -4,7 +4,7 @@ import { getApps, initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getStorage } from 'firebase-admin/storage';
 
-const BUCKET_NAME = "propel-lite.firebasestorage.app";
+const BUCKET_NAME = "propel-lite.appspot.com";
 
 // The Firebase Admin SDK automatically looks for the GOOGLE_APPLICATION_CREDENTIALS
 // environment variable. Ensure this variable is set to the path of your service account key.
