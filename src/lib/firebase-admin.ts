f2ddc9c459c0102372296fcc0cedf41475e22c90
paul osamai <paul.osamai8@gmail.com>
2025-07-27 05:33:02 +0000

    Frontend: Error during property creation or image upload: Error: Upload

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,19 +1,21 @@
 
 import admin from 'firebase-admin';
-import { getApps, getApp, initializeApp } from 'firebase-admin/app';
+import { getApps, initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getStorage } from 'firebase-admin/storage';
+
+const BUCKET_NAME = "propel-lite-9ed56.appspot.com";
 
 // The Firebase Admin SDK automatically looks for the GOOGLE_APPLICATION_CREDENTIALS
 // environment variable. Ensure this variable is set to the path of your service account key.
 
 if (!getApps().length) {
   initializeApp({
-    storageBucket: "propel-lite-9ed56.appspot.com"
+    storageBucket: BUCKET_NAME
   });
 }
 
 const db = getFirestore();
-const bucket = getStorage().bucket("propel-lite-9ed56.appspot.com");
+const bucket = getStorage().bucket(BUCKET_NAME);
 
 export { db, bucket, admin };
