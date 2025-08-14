34e68442f958984813e293f4d945ffb245958848
paul osamai <paul.osamai8@gmail.com>
2025-08-14 08:54:53 +0000

    // For Firebase JS SDK v7.20.0 and later, measurementId is optional cons

--- src/lib/firebase/client-app.ts
+++ src/lib/firebase/client-app.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -9,7 +10,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAh3_w39v-LhQ0txefaM2y6sn8C7tD6rqE",
   authDomain: "propel-lite-9ed56.firebaseapp.com",
   projectId: "propel-lite-9ed56",
-  storageBucket: "propel-lite-9ed56.appspot.com",
+  storageBucket: "propel-lite-9ed56.firebasestorage.app",
   messagingSenderId: "72284672505",
   appId: "1:72284672505:web:20c0c78093d5086d682d28",
   measurementId: "G-BB9NLCFL9B"
