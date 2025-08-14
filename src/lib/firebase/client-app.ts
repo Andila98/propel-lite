ec3af6f825852a5a2fdffb1068aec1fe8b5448dc
paul osamai <paul.osamai8@gmail.com>
2025-08-14 16:08:18 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase/client-app.ts
+++ src/lib/firebase/client-app.ts
@@ -5,14 +5,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  "projectId": "propel-lite",
-  "appId": "1:71781747717:web:98701b990b76c34f77606c",
-  "storageBucket": "propel-lite.firebasestorage.app",
-  "apiKey": "AIzaSyCcYPhpBKVFVnlsTAhK9lSH9sXQbshaid0",
-  "authDomain": "propel-lite.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "71781747717"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 
