29e6c53aa0133b3384043e484e76eefe508dda9d
paul osamai <paul.osamai8@gmail.com>
2025-08-24 18:18:08 +0000

    remove the emulator and connect to firebase directly

--- src/lib/firebase-client.ts
+++ src/lib/firebase-client.ts
@@ -1,8 +1,7 @@
-
 import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth, connectAuthEmulator } from 'firebase/auth';
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
-import { getStorage, connectStorageEmulator } from 'firebase/storage';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 import { firebaseConfig } from '@/config/firebase-config';
 
 // Initialize Firebase
@@ -11,13 +10,5 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const firestore = getFirestore(app);
 const storage = getStorage(app);
-
-// Connect to emulators if running locally
-if (typeof window !== 'undefined' && window.location.hostname === 'localhost') {
-  console.log('Connecting to Firebase emulators');
-  connectAuthEmulator(auth, 'http://localhost:9099', { disableRegenerateAuthHeader: true });
-  connectFirestoreEmulator(firestore, 'localhost', 8080);
-  connectStorageEmulator(storage, 'localhost', 9199);
-}
 
 export { auth, firestore, storage, app };
