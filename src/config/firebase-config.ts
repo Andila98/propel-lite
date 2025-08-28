6f1fb5ead51b92d2f1f160b7cdf83f114c238521
paul osamai <paul.osamai8@gmail.com>
2025-08-28 11:32:07 +0000

    update firebase with this "// Import the functions you need from the SDK

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -1,12 +1,10 @@
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyAh3_w39v-LhQ0txefaM2y6sn8C7tD6rqE",
-  authDomain: "propel-lite-9ed56.firebaseapp.com",
-  projectId: "propel-lite-9ed56",
-  storageBucket: "propel-lite-9ed56.appspot.com",
-  messagingSenderId: "72284672505",
-  appId: "1:72284672505:web:20c0c78093d5086d682d28",
-  measurementId: "G-BB9NLCFL9B"
+  apiKey: "AIzaSyCcYPhpBKVFVnlsTAhK9lSH9sXQbshaid0",
+  authDomain: "propel-lite.firebaseapp.com",
+  projectId: "propel-lite",
+  storageBucket: "propel-lite.appspot.com",
+  messagingSenderId: "71781747717",
+  appId: "1:71781747717:web:98701b990b76c34f77606c"
 };
