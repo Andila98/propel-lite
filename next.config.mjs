e6f614836b6f9ccbe661195be3a169f8bcadda1d
paul osamai <paul.osamai8@gmail.com>
2025-09-02 13:41:57 +0000

    Failed to load resources status 301 () api/auth/me:1

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'ohpverffrwflwksxoljv.supabase.co',
-        port: '',
-        pathname: '/**',
-      },
-       {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      }
-    ],
-  },
+  // trailingSlash: true, // This was causing API routes to redirect
 };
 
 export default nextConfig;
