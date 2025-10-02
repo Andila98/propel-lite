3fe0aedcb6201001aa69bb89108a05659d224fa2
paul osamai <paul.osamai8@gmail.com>
2025-10-02 15:47:45 +0000

    revoke the current dynamic links key and request for a new one

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -1,10 +1,12 @@
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyCcYPhpBKVFVnlsTAhK9lSH9sXQbshaid0",
+  apiKey: "REPLACE_WITH_YOUR_NEW_API_KEY",
   authDomain: "propel-lite.firebaseapp.com",
   projectId: "propel-lite",
   storageBucket: "propel-lite.appspot.com",
   messagingSenderId: "71781747717",
   appId: "1:71781747717:web:98701b990b76c34f77606c"
 };
+
