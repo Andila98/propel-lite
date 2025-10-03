b8f7cb250aa3a7dc7af9de90a75bf6aebdbac0b9
paul osamai <paul.osamai8@gmail.com>
2025-10-03 07:13:57 +0000

    i already have a firebase project

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -2,11 +2,10 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_NEW_API_KEY",
+  apiKey: "YOUR_VALID_API_KEY",
   authDomain: "propel-lite.firebaseapp.com",
   projectId: "propel-lite",
   storageBucket: "propel-lite.appspot.com",
   messagingSenderId: "71781747717",
   appId: "1:71781747717:web:98701b990b76c34f77606c"
 };
-
