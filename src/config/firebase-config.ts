81c0bfcf2212aa15d13d04c90b155f87d74e2d37
paul osamai <paul.osamai8@gmail.com>
2025-10-03 07:28:37 +0000

    add the firebase-config file to secrets and to gitignore

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -2,10 +2,11 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "YOUR_VALID_API_KEY",
-  authDomain: "propel-lite.firebaseapp.com",
-  projectId: "propel-lite",
-  storageBucket: "propel-lite.appspot.com",
-  messagingSenderId: "71781747717",
-  appId: "1:71781747717:web:98701b990b76c34f77606c"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
